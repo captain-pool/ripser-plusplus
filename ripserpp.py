139703e6295b2fe155020fcd62424a98b6bade11
captain-pool <rickdey1998@gmail.com>
2020-09-11 14:11:23 +0000

    It works but slow

--- ripserpp.py
+++ ripserpp.py
@@ -2,7 +2,19 @@ import numpy as np
 import io
 import subprocess
 import os
+import shlex
+from numba import njit
 
+def write_ltm(matrix, f):
+  with open(f, "w") as f:
+    x, y = np.tril_indices_from(matrix, -1)
+    a = x[0]
+    for idx in range(len(x)):
+      pair = x[idx], y[idx]
+      if a != pair[0]:
+        f.write("\n")
+        a = pair[0]
+      f.write("%f," % matrix[pair])
 
 def vietoris_rips_filteration(distance_matrix, dimension, verbose=False, ripserpp_bin_path="./build/ripser++"):
   ripserpp_bin_path = os.environ.get('RIPSERPP_BIN_PATH', ripserpp_bin_path)
@@ -15,15 +27,14 @@ def vietoris_rips_filteration(distance_matrix, dimension, verbose=False, ripserp
   if not np.all(np.diag(distance_matrix) == 0):
     raise Exception("Not a Distance Matrix!")
   devnull = None if verbose else open(os.devnull, "w")
-  ipipe = io.StringIO()
-  np.savetxt(ipipe, distance_matrix)
+  write_ltm(distance_matrix, "/tmp/distmat.txt")
+  command = ripserpp_bin_path + " --dim %d /tmp/distmat.txt" % dimension
+  command = shlex.split(command)
   proc = subprocess.Popen(
-      [ripserpp_bin_path, "--format", "distance", "--dim", str(dimension)],
+      command,
       stdout=subprocess.PIPE,
-      stdin=subprocess.PIPE,
       stderr=devnull)
-  ipipe.seek(0)
-  proc.stdin.write(ipipe.read().encode("utf-8"))
-  opipe = io.StringIO(proc.communicate()[0].decode("utf-8"))
-  proc.stdin.close()
+  opipe = io.BytesIO(proc.communicate()[0])
+  #ipipe.seek(0)
+  #proc.stdin.write(ipipe.read())
   return np.loadtxt(opipe).astype(np.int32)
