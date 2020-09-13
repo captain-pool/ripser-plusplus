bc3c6330583a05856c8d4a59d1e5f62f5c8be0b4
captain-pool <rickdey1998@gmail.com>
2020-09-13 09:38:12 +0000

    Using shared library for faster calculation

--- ripserpp.py
+++ ripserpp.py
@@ -4,6 +4,7 @@ import subprocess
 import os
 import shlex
 from numba import njit
+import sys
 
 def write_ltm(matrix, f):
   with open(f, "w") as f:
@@ -16,7 +17,15 @@ def write_ltm(matrix, f):
         a = pair[0]
       f.write("%f," % matrix[pair])
 
-def vietoris_rips_filteration(distance_matrix, dimension, verbose=False, ripserpp_bin_path="./build/ripser++"):
+def vietoris_rips_filteration(distance_matrix, dimension, verbose=False, pyripser_pp_bin="/rpp/python/build/libpyripser++.so"):
+    pyripser_pp_bin = os.environ['PYRIPSER_PP_BIN'] = os.environ.get('PYRIPSER_PP_BIN', pyripser_pp_bin)
+    sys.path.append(os.path.dirname(os.path.dirname(pyripser_pp_bin)))
+    import ripser_plusplus_python as rppp
+    rppp.run("--format distance --dim %d" % dimension, distance_matrix)
+    return np.loadtxt("/tmp/features.txt").astype(np.int32)
+
+
+def vietoris_rips_filteration_slow(distance_matrix, dimension, verbose=False, ripserpp_bin_path="/rpp/build/ripser++"):
   ripserpp_bin_path = os.environ.get('RIPSERPP_BIN_PATH', ripserpp_bin_path)
   if not ripserpp_bin_path  \
      or not os.path.exists(ripserpp_bin_path) \
@@ -27,7 +36,7 @@ def vietoris_rips_filteration(distance_matrix, dimension, verbose=False, ripserp
   if not np.all(np.diag(distance_matrix) == 0):
     raise Exception("Not a Distance Matrix!")
   devnull = None if verbose else open(os.devnull, "w")
-  write_ltm(distance_matrix, "/tmp/distmat.txt")
+  write_ltm(distance_matrix.astype("float16"), "/tmp/distmat.txt")
   command = ripserpp_bin_path + " --dim %d /tmp/distmat.txt" % dimension
   command = shlex.split(command)
   proc = subprocess.Popen(
